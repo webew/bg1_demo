593c728d0e6782aced598538d1ab8286b036633f
webew <fillion.bertrand@gmail.com>
2021-12-10 13:51:10 +0100

    burger visible

--- js/main.js
+++ js/main.js
@@ -1,18 +1,6 @@
 const burger = document.querySelector("#burger");
 const menu = document.querySelector("#menu_large ul");
-let menuMobileVisible = false;
 
 burger.addEventListener("click", function () {
-    if (menuMobileVisible) {
-        menu.style.display = "none";
-    } else {
-        // au premier clic, menu.style.display = ""
-        menu.style.display = "flex";
-    }
-    menuMobileVisible = !menuMobileVisible;
+    menu.classList.toggle('visible');
 });
-
-// burger.addEventListener("click", function() {
-//     menu.style.display = menuMobileVisible ? "none" : "flex";
-//     menuMobileVisible = !menuMobileVisible;
-// });
