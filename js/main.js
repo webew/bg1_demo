66deda1f6a55cfaf8582f60b62f27f147f2fa4d4
webew <fillion.bertrand@gmail.com>
2021-12-10 11:47:14 +0100

    burger correction

--- js/main.js
+++ js/main.js
@@ -2,7 +2,17 @@ const burger = document.querySelector("#burger");
 const menu = document.querySelector("#menu_large ul");
 let menuMobileVisible = false;
 
-burger.addEventListener("click", () => {
-    menu.style.display = menuMobileVisible ? "none" : "block";
+burger.addEventListener("click", function () {
+    if (menuMobileVisible) {
+        menu.style.display = "none";
+    } else {
+        // au premier clic, menu.style.display = ""
+        menu.style.display = "flex";
+    }
     menuMobileVisible = !menuMobileVisible;
 });
+
+// burger.addEventListener("click", function() {
+//     menu.style.display = menuMobileVisible ? "none" : "flex";
+//     menuMobileVisible = !menuMobileVisible;
+// });
